50de25109578aa6145352c63ad329a7915a62b4a
VadimReineke <reinekefox1999@gmail.com>
2023-11-29 15:20:52 +0300

    add check how many frinds for render sidebarFriends Arr

--- src/Components/Dialogs/DialogItem/DialogItem.jsx
+++ src/Components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from '../Dialogs.module.css';
 import { NavLink } from "react-router-dom";
-import { selectDialogCreator } from "../../../redux/store";
+import { selectDialogCreator } from "../../../redux/dialogs-reducer";
 
 
 
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -5,42 +5,41 @@ const ADD_POST = 'ADD-POST';
 
 
 // функция нахождения и присвоения нового id 
-const newId = (arr) =>{
-        let workArr = arr;
-        if (workArr.length === 0) {
-            let idMax = 1;
-            return idMax
-        } else {
-            let idMax = Math.max(...workArr.map(i => i.id));
-            idMax = idMax + 1;
-            return idMax
-        }
+const newId = (arr) => {
+    let workArr = arr;
+    if (workArr.length === 0) {
+        let idMax = 1;
+        return idMax
+    } else {
+        let idMax = Math.max(...workArr.map(i => i.id));
+        idMax = idMax + 1;
+        return idMax
+    }
 }
 
 const profileReducer = (state, action) => {
 
-switch(action.type) {
-  case UPDATE_TEXT_POST:    
-    state.postText = action.postText;
-    return state;
-case ADD_POST:
-    let postObj = {
-        id: newId(state.postData),
-        message: state.postText,
-        likesCount: 0
-    }
-    state.postData.push(postObj);
-    state.postText = '';
-    return state;
+    switch (action.type) {
+        case UPDATE_TEXT_POST:
+            state.postText = action.postText;
+            return state;
+        case ADD_POST:
+            let postObj = {
+                id: newId(state.postData),
+                message: state.postText,
+                likesCount: 0
+            }
+            state.postData.push(postObj);
+            state.postText = '';
+            return state;
 
-    default:
-        return state
+        default:
+            return state
     }
 }
 
-
 // Добавление постов
-export const onPostUpdateCreator = (newPostText) => ({type: UPDATE_TEXT_POST, postText: newPostText});
-export const addPostCreator = () => ({type: ADD_POST});
+export const onPostUpdateCreator = (newPostText) => ({ type: UPDATE_TEXT_POST, postText: newPostText });
+export const addPostCreator = () => ({ type: ADD_POST });
 
 export default profileReducer
