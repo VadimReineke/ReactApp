4094b937447c83f4edcab70388c21364092eb0d2
VadimReineke <reinekefox1999@gmail.com>
2024-03-16 18:08:43 +0300

    hook useState for profileStatus

--- src/Components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,51 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from './ProfileInfo.module.css';
 
+const ProfileStatus = (props) => {
 
-class ProfileStatus extends React.Component {
+    const[editMode, setEditMode] = useState(false);
 
-    state = {
-        editMode: false,
-        status: this.props.status
+    const activateMode = () => {
+        setEditMode(true);
     }
 
-    editModeStatus = () => {
-         this.setState({editMode: !this.state.editMode});
-         this.props.updateStatus(this.state.status);
+    const deactivateMode = () => {
+        setEditMode(false)
+        props.updateStatus(status);
     }
 
-    onStatusChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value
-        });
-    }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.status !== this.props.status) {
-            this.setState({
-                status: this.props.status
-            })
-        }
-       
-    }
+     const[status, setStatusChange] = useState(props.status);
 
-    render() {
-   
+     const onStatusChange = (e) => {
+            setStatusChange(e.currentTarget.value)
+     }
+
          return (
             <div>
-                {!this.state.editMode &&
+                {!editMode &&
                 <div>
-                    <span onClick={this.editModeStatus} className={classes.statusText}>{this.props.status || "Введите статус"}</span>
+                   
+                    <span onClick={activateMode} className={classes.statusText}>{props.status || "Введите статус"}</span>
                 </div>
                  }
-                 {this.state.editMode &&
+                 {editMode &&
                 <div>
-                    <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.editModeStatus} className={classes.statusTextInput} value={this.state.status}/>
+                    <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateMode} className={classes.statusTextInput} value={status}/>
                 </div>
                 }
             </div>
         )
-    }
+    
 }
 
 export default ProfileStatus
+
+
+
+// class ProfileStatus extends React.Component {
+
+//     state = {
+//         editMode: false,
+//         status: this.props.status
+//     }
+
+//     editModeStatus = () => {
+//          this.setState({editMode: !this.state.editMode});
+//          this.props.updateStatus(this.state.status);
+//     }
+
+//     onStatusChange = (e) => {
+//         this.setState({
+//             status: e.currentTarget.value
+//         });
+//     }
+
+//     componentDidUpdate(prevProps, prevState) {
+//         if (prevProps.status !== this.props.status) {
+//             this.setState({
+//                 status: this.props.status
+//             })
+//         }
+       
+//     }
+
+//     render() {
+   
+//          return (
+//             <div>
+//                 {!this.state.editMode &&
+//                 <div>
+//                     <span onClick={this.editModeStatus} className={classes.statusText}>{this.props.status || "Введите статус"}</span>
+//                 </div>
+//                  }
+//                  {this.state.editMode &&
+//                 <div>
+//                     <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.editModeStatus} className={classes.statusTextInput} value={this.state.status}/>
+//                 </div>
+//                 }
+//             </div>
+//         )
+//     }
+// }
